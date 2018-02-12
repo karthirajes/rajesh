6123108614ca1b0a130fb49d2c42744b3fba1d7a
karthirajes <35220312+karthirajes@users.noreply.github.com>
2018-02-12 15:46:45 +0530

    Create 154.c

---
+++ 154.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[10];
+scanf("%d",&a);
+int i,j,n,flag=0;
+for(i=o;i<=n/2;i++)
+{
+for(j=0;j=n-1`;j--)
+{
+ if(a[i]!=a[j])
+ {
+  printf("not palindrome")
+  break;
+  }
+  }
+}
+if(flag==0)
+	{
+		printf("palindrome");
+	}
+  return 0;
+}
