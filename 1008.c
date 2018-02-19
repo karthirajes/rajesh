c5e9973b34f8eef4f112dacfd4bbfeb42815685f
karthirajes <35220312+karthirajes@users.noreply.github.com>
2018-02-19 16:31:06 +0530

    Create 1008.c

---
+++ 1008.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+int a,b[100],c=0,i;
+printf("enter the numbers:");
+scanf("%d",&a);
+for(i=0;i<a;i++)
+{
+	scanf("%d",&b[i]);
+}
+for(i=0;i<a;i++)
+{
+c=c+b[i];
+}
+printf("\n%d",c);	
+return 0;                                                                                     
+}                     
